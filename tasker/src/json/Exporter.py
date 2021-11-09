b695b441934af0076cf5291c1e987a57d28e5ef2
sniochup <snioszekpawel@gmail.com>
2021-11-09 23:28:36 +0100

    Tasker exporter and importer file update

--- tasker/src/json/Exporter.py
+++ tasker/src/json/Exporter.py
@@ -8,6 +8,4 @@ class Exporter:
 
     def save_tasks(self, tasks):
         with open('taski.json', 'w', encoding="utf-8") as file:
-            json.dump(tasks, file)
-        file.close()
-        pass
+            json.dump(tasks, file, ensure_ascii=False, indent=2)
--- tasker/src/json/Importer.py
+++ tasker/src/json/Importer.py
@@ -5,14 +5,10 @@ class Importer:
 
     def __init__(self):
         self.data = {}
-        pass
 
     def read_tasks(self):
         with open('taski.json', 'r', encoding="utf-8") as file:
             self.data = json.load(file)
-        file.close()
-        pass
 
     def get_tasks(self):
         return self.data
-        pass
